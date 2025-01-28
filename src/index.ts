7971e3455d0fc20a75b3df1d45b66d8ed265cc0e
k4l3b4 <kaleba647@gmail.com>
2025-01-28 12:06:04 +0300

    feat(src): add index.ts for exports

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { DataFetcher } from './data-fetcher';
+export { InfiniteDataFetcher } from './infinite-data-fetcher';
+
+export { fetchWithSettings } from './lib/fetcher';
+
+export { buildQueryString } from './lib/utils';
+
+export { DataFetcherProvider, useFetcherSettings } from './provider';
+
+export type { DataFetcherProps, InfiniteDataFetcherProps } from './types';
