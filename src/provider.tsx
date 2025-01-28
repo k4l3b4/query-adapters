80811a5b8c683e7f3033d30137a88e1c49675b43
k4l3b4 <kaleba647@gmail.com>
2025-01-28 12:05:49 +0300

    feat(api): add DataFetcherProvider and useFetcherSettings hooks

---
+++ src/provider.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, type ReactNode } from 'react'
+
+
+// this provider will be used to set the default settings for the fetcher
+// if ya'll don't want to pass a queryfn each time and your mostly
+// using it to pull data from an api or a url in general
+
+interface FetcherSettings extends RequestInit {
+    baseUrl?: string; // Optional base URL for requests
+}
+
+// Default settings for the fetcher
+const defaultSettings: FetcherSettings = {
+    baseUrl: '',
+    headers: { 'Content-Type': 'application/json' },
+};
+
+const FetcherContext = createContext<FetcherSettings>(defaultSettings);
+
+interface DataFetcherProviderProps {
+    settings: FetcherSettings;
+    children: ReactNode;
+}
+
+export const DataFetcherProvider: React.FC<DataFetcherProviderProps> = ({
+    settings,
+    children,
+}) => {
+    return (
+        <FetcherContext.Provider value={{ ...defaultSettings, ...settings }}>
+            {children}
+        </FetcherContext.Provider>
+    );
+};
+
+export const useFetcherSettings = () => {
+    return useContext(FetcherContext);
+};
